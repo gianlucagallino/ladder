eedae6bb174bf790c54144d332ff043c1bdfba01
ggal04 <gianlucaegallino@gmail.com>
2023-05-30 22:31:35 -0300

    Add Gamescreen

--- ladder.cpp
+++ ladder.cpp
@@ -2,18 +2,34 @@
 #include "rlutil.h"
 using namespace std;
 
+//TO DO:
+//Arreglar bug linea 68
+//Mejorar instrucciones
+//Crear modo 1 jugador
+//Crear modo 2 jugadores
+// Colorar la consola, centrar el ascii
+//Midscreen
+//Game logic, resto de tiros posibles
+//Agregar y poner el resto de variables.
+
 int main() 
 {
-    int highscore = 0, pick = 0;
-        cout << "    __           _     _                " <<endl;
-        cout << "   / /  __ _  __| | __| | ___ _ __ ___  " <<endl;
-        cout << "  / /  / _` |/ _` |/ _` |/ _ ' '__/ __| " <<endl;
-        cout << " / /__| (_| | (_| | (_| |  __/ |  '__ ' " <<endl;
-        cout << " |____/'__,_|'__,_|'__,_|'___|_|  |___/ " <<endl;
-        cout << "                                        " <<endl;
-        cout << "       Press any key to continue!       " <<endl;
-        rlutil::anykey();
-        rlutil::cls();
+    int highscore = 0, pick = 0, play = 0;
+    string player1, player2;
+    char dice[5] = {⚀,⚁,⚂,⚃,⚄,⚅}
+    int rscores[5] = {0}
+
+    cout << "    __           _     _                " <<endl;
+    cout << "   / /  __ _  __| | __| | ___ _ __ ___  " <<endl;
+    cout << "  / /  / _` |/ _` |/ _` |/ _ ' '__/ __| " <<endl;
+    cout << " / /__| (_| | (_| | (_| |  __/ |  '__ ' " <<endl;
+    cout << " |____/'__,_|'__,_|'__,_|'___|_|  |___/ " <<endl;
+    cout << "                                        " <<endl;
+    cout << "       Press any key to continue!       " <<endl;
+    rlutil::anykey();
+    rlutil::cls();
+
+    //MENU AND INTERACTION
 
     while (pick  == 0 || pick == 3)
     {
@@ -27,7 +43,7 @@ int main()
         cout << "                                " <<endl;
         cout << "          HIGHSCORE: " << highscore <<endl;
         cout << "                                " <<endl;
-        cout << "Choose an option: "; pick= 3; cin >> pick;
+        cout << "Choose an option: "; cin >> pick;
 
         switch (pick)
         {
@@ -38,7 +54,7 @@ int main()
             break;
 
         case 3: 
-            rlutil:: cls();
+            rlutil::cls();
             cout << " ______________________________ " <<endl;
             cout << "|                              |" <<endl;
             cout << "|             RULES:           |" <<endl;
@@ -69,5 +85,45 @@ int main()
             break;
         }
     }
+
+    // NAME STRING INPUT
+
+    if (pick == 1)
+    {
+        cout << "Enter player name: ";
+        cin >> player1;
+    }
+    else if (pick == 2)
+    {
+        cout << "Enter first player name: ";
+        cin >> player1;
+        cout << "Enter second player name: ";
+        cin >> player2;
+    }
+
+    //Game logic
+
+    //Round layout
+    while (play != 3)
+    {
+        rlutil::cls();
+        cout << playerturn <<"'s turn | Round " << rnd <<" | Score:" <<score <<" |" <<endl;
+        cout << "_________________________________________________________________" <<endl;
+        cout << "Throw n°" << play <<endl;
+        cout << "Current score:°" << cscore <<endl;
+        cout << "_________________________________________________________________" <<endl;
+        cout << rscores[0] <<" " << rscores[1] <<" "<< rscores[2] <<" "<< rscores[3] <<" "<< rscores[4] <<" " << rscores[5] <<endl;
+        cout << " " <<endl;
+        cout << message <<endl;
+        rlutil::anykey();
+    }
+        if (valid)
+    {
+        score += cscore;
+    }
+
+    //midscreen
+
+
   return 0;
 }
